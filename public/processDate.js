5131fe4659e96ea30f1192ffebdb61a27f243608
Sam Beckett <becketts@byu.edu>
2020-06-04 14:14:49 -0600

    improve scanList UI

--- public/processDate.js
+++ public/processDate.js
@@ -1,3 +1,7 @@
+/* eslint-disable */
+
 window.onload = (event) => {
-  Array.from(document.getElementsByClassName('date-item')).forEach((el) => el.innerHTML = (new Date(el.innerHTML)).toLocaleString());
+  Array.from(document.getElementsByClassName('date-item')).forEach((el) => {
+    el.innerHTML = moment(el.innerHTML).format('MM/DD/YY, HH:MM A');
+  });
 };
